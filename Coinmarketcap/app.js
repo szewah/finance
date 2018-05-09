c795e6e44b116cfe617fa9b98081394c471f9ef8
Szewah <swgrleong@gmail.com>
2018-05-09 15:10:24 -0400

    coinmarketcap_1

---
+++ Coinmarketcap/app.js
@@ -0,0 +1,14 @@
+let coinMarketCap_API = "https://api.coinmarketcap.com";
+let updateInterval = 60 * 1000;
+
+let app = new Vue ({
+	el: "#app",
+	data: {
+		coins: [],
+		coinData: {}
+	},
+})
+
+	methods: {
+		
+	}
