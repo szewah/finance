a70dd70c57c43f8268b9352f4c00e2515c8e2497
Szewah <swgrleong@gmail.com>
2018-05-21 14:23:56 -0400

    coinmarketcap_app

--- Coinmarketcap/app.js
+++ Coinmarketcap/app.js
@@ -1,40 +1,50 @@
-let coinMarket = 'https://api.coinmarketcap.com'
+let coinMarket = 'https://api.coinmarketcap.com/v2/ticker/?limit=10'
 let updateInterval = 60 * 1000;
 
-let newApp = new Vue ({
-	el: '#coinApp',
-	data: {
-		// data within objects
-		results: {}
-	},
-
-	methods: { 
-
-		getCoins: function () {
-		axios
-			.get(coinMarket +'/v2/ticker/?limit=10')
-  			.then((resp) => {
-				this.results = resp.data.data
-    			// console.log(resp.data); 
-    			// console.log(resp.status); 
-  				}); 
-		},
-
-		getColor: (num) => {
-      		return num > 0 ? "color:green;" : "color:red;";
-    	},
-    
+let newApp = new Vue({
+    el: '#coinApp',
+    data: {
+        // data within objects
+        results: []
     },
 
-    created: function () {
-    	this.getCoins();
-  	}
+    methods: {
+
+        getCoins: function() {
+            axios
+                .get(coinMarket)
+                .then((resp) => {
+                    // this.results = resp.data.data
+                    this.results = formatCoins(resp);
+                    debugger
+                });
+        },
+
+        getColor: (num) => {
+            return num > 0 ? "color:green;" : "color:red;";
+        },
+    },
+
+    created: function() {
+        this.getCoins();
+    }
 })
 
+
 setInterval(() => {
-  	newApp.getCoins();
-}, 
-  	updateInterval
+        newApp.getCoins();
+    },
+    updateInterval
 );
 
-
+function formatCoins(response) {
+    var coinDataArray = []
+    Object.keys(response.data).forEach(function(key) {
+        coinDataArray.push(response.data[key])
+    })
+    coinDataArray.sort(function(a, b) {
+        return a.rank > b.rank;
+    })
+    console.log(coinDataArray)
+    debugger
+}
